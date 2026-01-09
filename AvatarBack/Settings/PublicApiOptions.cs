0f375012279318d79b04ed97fad78b347b64d021
metafusionSAS <administrador@metafusion.com.co>
2026-01-09 09:21:25 -0500

    Add public API key validation for announce endpoints

---
+++ AvatarBack/Settings/PublicApiOptions.cs
@@ -0,0 +1,8 @@
+namespace Avatar_3D_Sentry.Settings;
+
+public class PublicApiOptions
+{
+    public const string SectionName = "PublicApi";
+
+    public string ApiKey { get; set; } = string.Empty;
+}
